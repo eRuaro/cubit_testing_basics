2b3332da900b35441a640f8fcf4f792b38c90ca3
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-06-17 15:44:38 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/cubit_testing_basics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cubit_testing_basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Widget _circleButton(Function onPressed, String operator) {
+    return CircleAvatar(
+      backgroundColor: Colors.blueAccent,
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            onPressed;
+          });
+        },
+        child: Text(
+          operator,
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Row(
+              children: [
+              
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
